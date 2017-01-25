b0473e9c83b5a2ff141077708989a49c78a52440
Aymeric-Duplaquet <aymeric.duplaquet1@uqac.ca>
2017-01-25 00:06:33 -0500

    Add base IU (#3)
    * IU
    
    Option Menu + button. Shorcut : esc
    Speed panel and speed update
    Info panel placeholder for futur information IU
    
    * Petites corrections UI.
    
    * Après avoir save la scene c'est mieu
    
    * Comment on scripts
    
    * Project speed to front axis

---
+++ Assets/Script/OptionMenuClick.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionMenuClick : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// Save the progression. 
+    /// TODO
+    /// </summary>
+    public void OnSave()
+    {
+        //TODO
+    }
+
+    /// <summary>
+    /// Return to the main menu
+    /// </summary>
+    public void OnReturnMainMenu()
+    {
+        //TODO
+    }
+
+    /// <summary>
+    /// Use when the user open the in game menu. 
+    /// Open the menu, pause the game. Unlock the cursor.
+    /// </summary>
+    public void OnLaunchMenu()
+    {
+        gameObject.SetActive(true);
+        //TODO : Pause game, unlock cursor
+    }
+
+    /// <summary>
+    /// Use when the user clik on the resume menu button
+    /// Resume the game, close the menu
+    /// </summary>
+    public void OnResumeGame()
+    {
+        gameObject.SetActive(false);
+        //TODO : UnpauseGame, lock cursor
+    }
+    /// <summary>
+    /// Use when the user clik on the Quit menu button
+    /// </summary>
+    public void OnQuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Script/TempoTestAcceleration.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempoTestAcceleration : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody>().velocity = new Vector3(100, 0, 0);
+	}
+}
