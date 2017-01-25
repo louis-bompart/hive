73e671f5da733672d5bb35d779dd748ba63b475d
Kyvi <taovincentl@gmail.com>
2017-01-25 00:08:31 -0500

    Add skybox and random Asteroids generation (#2)
    * Skybox + Asteroids
    
    * Comments

---
+++ Assets/Scripts/AsteroidsBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidsBehavior : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	private int minrSpeed = 1;
+	private int maxrSpeed = 8;
+	private int rxSpeed;
+	private int rySpeed;
+	private int rzSpeed;
+
+	private int minmSpeed = 1;
+	private int maxmSpeed = 20;
+	private int mxSpeed;
+	private int mySpeed;
+	private int mzSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody>();
+
+		Vector3 movement = new Vector3 (mxSpeed, mySpeed, mzSpeed);
+		rb.AddForce (movement);
+
+		mxSpeed = Random.Range (minmSpeed, maxmSpeed);
+		mySpeed = Random.Range (minmSpeed, maxmSpeed);
+		mzSpeed = Random.Range (minmSpeed, maxmSpeed);
+
+		rxSpeed = Random.Range (minrSpeed, maxrSpeed);
+		rySpeed = Random.Range (minrSpeed, maxrSpeed);
+		rzSpeed = Random.Range (minrSpeed, maxrSpeed);
+
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate (new Vector3 (rxSpeed, rySpeed, rzSpeed) * Time.deltaTime);
+
+	}
+}
---
+++ Assets/Scripts/AsteroidsGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidsGenerator : MonoBehaviour {
+
+	public GameObject[] asteroids; 
+	private Vector3 origin = Vector3.zero;
+
+	private float minSize = 0.2f;
+	private float maxSize = 1.5f;
+
+	private int minCount = 500;
+	private int maxCount = 1200;
+
+	private float minDistance = 30.0f;
+	private float maxDistance = 150.0f;
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	 void Start () {
+		origin = transform.position;
+		GenerateAsteroids(Random.Range(minCount, maxCount));
+	}
+
+	/// <summary>
+	/// Generates the asteroids.
+	/// </summary>
+	/// <param name="asteroidsCount">Asteroids count.</param>
+	public void GenerateAsteroids (int asteroidsCount) {
+		for (int i = 0; i < asteroidsCount; i++) {
+			float size = Random.Range(minSize, maxSize);
+			GameObject prefab = asteroids[Random.Range(0, asteroids.Length)];
+			Vector3 position = Vector3.zero;
+
+			for (int j = 0; j < 100; j++) {
+				 position = Random.insideUnitSphere * (minDistance + (maxDistance - minDistance) * Random.value);
+				position += origin;
+				if (!Physics.CheckSphere(position, size / 2.0f)) {
+					break;
+				}
+			}
+			GameObject go = Instantiate(prefab, position, Random.rotation);
+			go.transform.localScale = new Vector3(size, size, size);
+		}
+	}
+}
