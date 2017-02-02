afb05016803e028f3d405bd94334808b9d848ba4
Pixel <louis.vincent.bompart@gmail.com>
2017-02-02 18:47:09 -0500

    Sprint 02 (#18)
    * Add basic movements to the ship (#4)
    
    * Add base IU (#3)
    
    * IU
    
    Option Menu + button. Shorcut : esc
    Speed panel and speed update
    Info panel placeholder for futur information IU
    
    * Petites corrections UI.
    
    * Après avoir save la scene c'est mieu
    
    * Comment on scripts
    
    * Project speed to front axis
    
    * Add skybox and random Asteroids generation (#2)
    
    * Skybox + Asteroids
    
    * Comments
    
    * Getting things in order
    
    * Cleaning of the ship controlling script (#6)
    
    * Add debug colors
    
    RGB, the classics
    
    * Add basic ship controls
    
    * Cleaning code
    
    * Fixes on Asteroids and documenting sources (#5)
    
    * Skybox + Asteroids
    
    * Comments
    
    * Comments added. Private attributes -> Public.
    Mesh Colliders added to Asteroids.
    
    * Cleaning - final
    
    * Add spaceship 3D model
    
    Model of the spaceship made with Maya (fbx)
    
    * Sprint01-Build
    
    Last fixes :
     * Put mesh collider again on asteroids after a mishap in source control
     * Add space ship model + mesh collider -inflated for performance sake-
     * Clean the dev stuff and create the release scene
    
    * Fix player's ship
    
    The acceleration is now computed independantly of the frame-refreshing
    
    * Mining lazer (#15)
    
    * Sprint01 - Build (#9)
    
    * Add basic steering of the ship
    * Add a first iteration of the 3d-model of the ship
    * Add random asteroid fields
    * Add a basic UI displaying speed and a menu
    
    * Implementation sommaire du laser
    
    * Preparation au merge
    
    Creation du prefab "ShipCannon"
    
    * Suppression fichiers
    
    * Asteroid mineral, destruction (#17)
    
    * Remodel Asteroids + Destruction and Mining
    
    * Particul System of Asteroid Desintegration
    
    * Graphic changes (#16)
    
    New Camera
    New Reticle 
    New effects on engine
    
    * Re-set controls
    
    Merge happens
    
    * Merged branch dev into Super-dev
    Add cannon to ship
    
    
    Update trails
    
    
    Merged branch Super-dev into dev
    Merge branch 'dev'
    
    Conflicts:
    	Assets/Scenes/Release.unity
    	Assets/Scripts/PlayerMovement.cs
    	ProjectSettings/InputManager.asset
    
    Complete ship's trails

---
+++ Assets/Scripts/Ship/MiningLaserScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiningLaserScript : MonoBehaviour {
+
+    LineRenderer line;
+    GameObject particleObject;
+    ParticleSystem particle;
+    ParticleSystem.EmissionModule em;
+
+    void Start ()
+    {
+        line = gameObject.GetComponent<LineRenderer>();
+        line.enabled = false;
+        particleObject = Instantiate(gameObject);
+        Destroy(particleObject.GetComponent<LineRenderer>());
+        Destroy(particleObject.GetComponent<MiningLaserScript>());
+        particle = particleObject.GetComponent<ParticleSystem>();
+        em = particle.emission;
+        em.enabled = false;
+        gameObject.GetComponent<ParticleSystem>().Stop();
+    }
+	
+	void Update ()
+    {
+		if(Input.GetButtonDown("Fire2"))
+        {
+            StopCoroutine("FireMiningLaser");
+            StartCoroutine("FireMiningLaser");
+        }
+	}
+    
+    IEnumerator FireMiningLaser()
+    {
+        line.enabled = true;
+        while(Input.GetButton("Fire2"))
+        {
+            line.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, Time.time);
+            Ray miningray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            line.SetPosition(0, miningray.origin);
+
+            if (Physics.Raycast(miningray, out hit, 8))
+            {
+                line.SetPosition(1, hit.point);
+                particleObject.transform.position = hit.point;
+                em.enabled = true;
+            }                
+            else
+            {
+                line.SetPosition(1, miningray.GetPoint(8));
+                em.enabled = false;
+            }
+
+
+            yield return null;
+        }
+
+        line.enabled = false;
+        em.enabled = false;
+    }
+}
