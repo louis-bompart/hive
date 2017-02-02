a4e4385f9c9945f6ac949732911719ab66587ab8
Pixel <louis.vincent.bompart@gmail.com>
2017-02-02 18:45:41 -0500

    Merged branch dev into Super-dev Add cannon to ship
    Update trails
    
    
    Merged branch Super-dev into dev
    Merge branch 'dev'
    
    Conflicts:
    	Assets/Scenes/Release.unity
    	Assets/Scripts/PlayerMovement.cs
    	ProjectSettings/InputManager.asset
    
    Complete ship's trails

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,21 +8,45 @@ public class PlayerMovement : MonoBehaviour
     public float AuxThrust = 2.5f;
     public float Torque = 2.5f;
 
+    private float drag = 0.0f;
+    private float angDrag = 0.0f;
+
     public bool isYawInverted = false;
     public bool isPitchInverted = true;
     public bool isRollInverted = true;
+
+    public float deadZoneX = 0.15f;
+    public float deadZoneY = 0.15f;
+
+    public bool showCursor = false;
+
+    public EnginesAnimation engineAnimation;
 
     private Rigidbody rb;
 
     // Use this for initialization
     void Start()
     {
+        engineAnimation = GetComponentInChildren<EnginesAnimation>();
         rb = GetComponent<Rigidbody>();
+        Cursor.visible = showCursor;
+        drag = rb.drag;
+        angDrag = rb.angularDrag;
     }
 
     // Update is called once per frame
     void Update()
     {
+        HandleFAInput();
+    }
+    
+    void HandleFAInput()
+    {
+        if(Input.GetAxis("Flight Assist") > 0)
+        {
+            rb.drag = Mathf.Abs(rb.drag - drag);
+            rb.angularDrag = Mathf.Abs(rb.angularDrag - angDrag);
+        }
     }
 
     void FixedUpdate()
@@ -37,16 +61,34 @@ public class PlayerMovement : MonoBehaviour
     /// <summary>
     /// Handle inputs for player's ship orientation
     /// </summary>
-    /// <returns>The reauested torque</returns>
+    /// <returns>The requested torque</returns>
     private Vector3 ComputeTorques()
     {
         Vector3 torque = Vector3.zero;
         // TODO: Reverse w/ aux thrust
 
         torque += Vector3.forward * Input.GetAxis("Roll") * Torque * (isRollInverted ? -1 : 1);
-        torque += Vector3.up * Input.GetAxis("Yaw") * Torque * (isYawInverted ? -1 : 1);
-        torque += Vector3.right * Input.GetAxis("Pitch") * Torque * (isPitchInverted ? -1 : 1);
+        Vector3 mouse = GetMouse();
+        torque += Vector3.up * mouse.x * Torque * (isYawInverted ? -1 : 1);
+        torque += Vector3.right * mouse.y * Torque * (isPitchInverted ? -1 : 1);
         return torque;
+    }
+
+    /// <summary>
+    /// Compute the relative position of the mouse w/ the center and it's dead zone as ref.
+    /// </summary>
+    /// <returns>A vector3 w/ x for the width, y for the height and z as nothing</returns>
+    private Vector3 GetMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        //If mousePosition.x is in the deadzone then consider it to be at 0;
+        mousePosition.x = Mathf.Abs(mousePosition.x) > deadZoneX*Screen.width/2 ? mousePosition.x : 0;
+        //Raw pixel value to percent ([0,1])
+        mousePosition.x /= (Screen.width / 2);
+        //Same for mousePosition.y
+        mousePosition.y = Mathf.Abs(mousePosition.y) > deadZoneY*Screen.height/2 ? mousePosition.y : 0;
+        mousePosition.y /= (Screen.height / 2);
+        return mousePosition;
     }
 
     /// <summary>
@@ -59,6 +101,7 @@ public class PlayerMovement : MonoBehaviour
         acceleration += Vector3.forward * Input.GetAxis("Thrust") * MainThrust;
         acceleration += Vector3.up * Input.GetAxis("Vertical") * AuxThrust;
         acceleration += Vector3.right * Input.GetAxis("Horizontal") * AuxThrust;
+        engineAnimation.UpdateThrottle(Vector3.Project(acceleration,Vector3.forward).magnitude / MainThrust +0.11f);
         return acceleration;
     }
 }
