6908f445619d9e8360e67fef4772c88e8aa9b830
Aymeric-Duplaquet <aymeric.duplaquet1@uqac.ca>
2017-03-08 14:53:16 -0500

    Add base upgrades (#34)
    * Fixes (#19)
    
    * Add basic movements to the ship (#4)
    
    * Add base IU (#3)
    
    * IU
    
    Option Menu + button. Shorcut : esc
    Speed panel and speed update
    Info panel placeholder for futur information IU
    
    * Petites corrections UI.
    
    * Après avoir save la scene c'est mieu
    
    * Comment on scripts
    
    * Project speed to front axis
    
    * Add skybox and random Asteroids generation (#2)
    
    * Skybox + Asteroids
    
    * Comments
    
    * Getting things in order
    
    * Cleaning of the ship controlling script (#6)
    
    * Add debug colors
    
    RGB, the classics
    
    * Add basic ship controls
    
    * Cleaning code
    
    * Fixes on Asteroids and documenting sources (#5)
    
    * Skybox + Asteroids
    
    * Comments
    
    * Comments added. Private attributes -> Public.
    Mesh Colliders added to Asteroids.
    
    * Cleaning - final
    
    * Add spaceship 3D model
    
    Model of the spaceship made with Maya (fbx)
    
    * Sprint01-Build
    
    Last fixes :
     * Put mesh collider again on asteroids after a mishap in source control
     * Add space ship model + mesh collider -inflated for performance sake-
     * Clean the dev stuff and create the release scene
    
    * Fix player's ship
    
    The acceleration is now computed independantly of the frame-refreshing
    
    * Mining lazer (#15)
    
    * Sprint01 - Build (#9)
    
    * Add basic steering of the ship
    * Add a first iteration of the 3d-model of the ship
    * Add random asteroid fields
    * Add a basic UI displaying speed and a menu
    
    * Implementation sommaire du laser
    
    * Preparation au merge
    
    Creation du prefab "ShipCannon"
    
    * Suppression fichiers
    
    * Asteroid mineral, destruction (#17)
    
    * Remodel Asteroids + Destruction and Mining
    
    * Particul System of Asteroid Desintegration
    
    * Graphic changes (#16)
    
    New Camera
    New Reticle 
    New effects on engine
    
    * Re-set controls
    
    Merge happens
    
    * Merged branch dev into Super-dev
    Add cannon to ship
    
    
    Update trails
    
    
    Merged branch Super-dev into dev
    Merge branch 'dev'
    
    Conflicts:
    	Assets/Scenes/Release.unity
    	Assets/Scripts/PlayerMovement.cs
    	ProjectSettings/InputManager.asset
    
    Complete ship's trails
    
    * 2 CC of fixes
    
    Avoid 60% Usage of CPU for physics
    And fix rocks collisions
    
    * Data singleton and few UI for StorageBay
    
    * Shield
    
    * Change shield mat

---
+++ Assets/Scripts/Data/Data.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+    #region SingletonPatern
+    private static Data _instance;
+
+    public static Data instance
+    {
+        get
+        {
+            if(_instance==null)
+            {
+                Debug.Log("ForcedDataInitialisation");
+                GameObject data = new GameObject("Data");
+                data.AddComponent<Data>();
+                _instance = data.GetComponent<Data>();
+            }
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    #region Inventory Data
+    //TODO
+
+    #endregion
+
+    #region Base Data
+        
+    #endregion
+
+    private void initialise()
+    {
+        //Conservation of the new gameObject on load
+        GameObject.DontDestroyOnLoad(gameObject);
+
+
+    }
+
+    public void Awake()
+    {
+        _instance = this;
+        initialise();
+    }
+
+
+}
---
+++ Assets/Scripts/Entity/Shield.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : Entity
+{
+    //In health/seconde
+    public float shieldRechargeRate;
+
+
+    // Use this for initialization
+    protected override void Start () {
+		
+	}
+
+    // Update is called once per frame
+    protected override void Update () {
+        //TODO : recharge of the shield over time
+	}
+}
