96b8e1ce767419b3e807174b07faaff8730d969c
Moonvibes <antoine.ruffion@gmail.com>
2017-03-10 22:56:15 -0500

    Card cleanup

--- Assets/Scripts/Base/BaseAccess.cs
+++ Assets/Scripts/Base/BaseAccess.cs
@@ -10,6 +10,36 @@ public class BaseAccess : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other)
     {
+        Button_Prompt.enabled = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Cursor.visible = true;
+            SceneManager.LoadSceneAsync("Bridge", LoadSceneMode.Single);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Button_Prompt.enabled = false;
+    }
+}
+
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BaseAccess : MonoBehaviour {
+
+    public Text Button_Prompt;
+
+    private void OnTriggerEnter(Collider other)
+    {
         Button_Prompt.gameObject.SetActive(true);
     }
 
