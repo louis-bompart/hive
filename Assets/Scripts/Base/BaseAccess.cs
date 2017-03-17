8305f4db36badd72d60954953796dfe28652a6c9
Pixel <louis.vincent.bompart@gmail.com>
2017-03-17 17:26:01 -0400

    Fix baseAccess

--- Assets/Scripts/Base/BaseAccess.cs
+++ Assets/Scripts/Base/BaseAccess.cs
@@ -1,39 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-
-public class BaseAccess : MonoBehaviour {
-
-    public Text Button_Prompt;
-    private void Start()
-    {
-        Button_Prompt.enabled = false;
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        Button_Prompt.enabled = true;
-        Debug.Log("Possible de rentrer dans la base.");
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (Input.GetButtonDown("Interact"))
-        {
-            Cursor.visible = true;
-            Debug.Log("Scene loading...");
-            SceneManager.LoadScene("Hangar", LoadSceneMode.Single);
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        Button_Prompt.enabled = false;
-    }
-}
-
-//﻿using System.Collections;
+﻿//using System.Collections;
 //using System.Collections.Generic;
 //using UnityEngine;
 //using UnityEngine.UI;
@@ -42,23 +7,59 @@ public class BaseAccess : MonoBehaviour {
 //public class BaseAccess : MonoBehaviour {
 
 //    public Text Button_Prompt;
-
+//    private void Start()
+//    {
+//        Button_Prompt.enabled = false;
+//    }
 //    private void OnTriggerEnter(Collider other)
 //    {
-//        Button_Prompt.gameObject.SetActive(true);
+//        Button_Prompt.enabled = true;
+//        Debug.Log("Possible de rentrer dans la base.");
 //    }
 
 //    private void OnTriggerStay(Collider other)
 //    {
-//        if (Input.GetAxis("Submit")>0)
+//        if (Input.GetButtonDown("Interact"))
 //        {
 //            Cursor.visible = true;
-//            SceneManager.LoadSceneAsync("Bridge", LoadSceneMode.Single);
+//            Debug.Log("Scene loading...");
+//            SceneManager.LoadScene("Hangar", LoadSceneMode.Single);
 //        }
 //    }
 
 //    private void OnTriggerExit(Collider other)
 //    {
-//         Button_Prompt.gameObject.SetActive(false);
+//        Button_Prompt.enabled = false;
 //    }
 //}
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BaseAccess : MonoBehaviour
+{
+
+    public Text Button_Prompt;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Button_Prompt.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetAxis("Submit") > 0)
+        {
+            Cursor.visible = true;
+            SceneManager.LoadSceneAsync("Bridge", LoadSceneMode.Single);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Button_Prompt.gameObject.SetActive(false);
+    }
+}
