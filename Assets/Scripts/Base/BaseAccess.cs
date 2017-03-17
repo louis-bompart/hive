083e69efa1c7090c526fa0ba7a7938285fbc048b
Pixel <louis.vincent.bompart@gmail.com>
2017-03-17 15:54:34 -0400

    Merge fixes

--- Assets/Scripts/Base/BaseAccess.cs
+++ Assets/Scripts/Base/BaseAccess.cs
@@ -33,32 +33,32 @@ public class BaseAccess : MonoBehaviour {
     }
 }
 
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+//﻿using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using UnityEngine.SceneManagement;
 
-public class BaseAccess : MonoBehaviour {
+//public class BaseAccess : MonoBehaviour {
 
-    public Text Button_Prompt;
+//    public Text Button_Prompt;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        Button_Prompt.gameObject.SetActive(true);
-    }
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        Button_Prompt.gameObject.SetActive(true);
+//    }
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (Input.GetAxis("Submit")>0)
-        {
-            Cursor.visible = true;
-            SceneManager.LoadSceneAsync("Bridge", LoadSceneMode.Single);
-        }
-    }
+//    private void OnTriggerStay(Collider other)
+//    {
+//        if (Input.GetAxis("Submit")>0)
+//        {
+//            Cursor.visible = true;
+//            SceneManager.LoadSceneAsync("Bridge", LoadSceneMode.Single);
+//        }
+//    }
 
-    private void OnTriggerExit(Collider other)
-    {
-         Button_Prompt.gameObject.SetActive(false);
-    }
-}
+//    private void OnTriggerExit(Collider other)
+//    {
+//         Button_Prompt.gameObject.SetActive(false);
+//    }
+//}
