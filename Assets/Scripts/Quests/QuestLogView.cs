cfade958b0661f05e2fbe8327dfd6118e6c53d7c
Moonvibes <antoine.ruffion@gmail.com>
2017-04-01 20:18:41 -0400

    Continuation of work on quest system
    -Finalized bridge interface
    -Prepared QuestLogController, View and model for a MVC implementation of the QuestLog

---
+++ Assets/Scripts/Quests/QuestLogView.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestLogView : MonoBehaviour {
+
+    public QuestLogModel questlogmodel;
+    public Quest[] quests;
+
+    void Awake()
+    {
+        questlogmodel = null;
+        questlogmodel = GameObject.Find("QuestLog").GetComponent<QuestLogModel>();
+    }
+
+    private void Start()
+    {
+        LoadAllQuests();
+    }
+
+    public void LoadAllQuests()
+    {
+        Dictionary<Quest, int> questLogDico = questlogmodel.QuestLog;
+        foreach(Quest quest in quests)
+    }
+}
