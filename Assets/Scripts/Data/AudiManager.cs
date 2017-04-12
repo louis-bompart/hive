dc3351fee3d6acfe10d951b0887188940f9d27b4
Aymeric-Duplaquet <aymeric.duplaquet1@uqac.ca>
2017-04-12 02:52:23 -0400

    Audio management (#69)
    Il y a des modif sur les scène pour virer les anciennes audio source des musiques.
    Pas forcément important mais il faudrat du coup enlever a nous les audio source si ces changement sont delete

---
+++ Assets/Scripts/Data/AudiManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudiManager : MonoBehaviour {
+    public List<AudioClip> SpaceMusics;
+    public AudioClip baseMusique;
+    bool inBase;
+
+    private AudioSource source;
+    // Use this for initialization
+    void Start () {
+        source = GetComponent<AudioSource>();
+
+        if(testInBase())
+        {
+            launchBaseMusique();
+            inBase = true;
+        }
+        else
+        {
+            lauchSpaceMusique();
+            inBase = false;
+        }
+    }
+	
+	
+    
+    // Update is called once per frame
+	void Update () {
+	    if(!inBase && testInBase())
+        {
+            inBase = true;
+            launchBaseMusique();
+            Debug.Log("inBase");
+        }
+        else if(inBase && !testInBase())
+        {
+            inBase = false;
+            lauchSpaceMusique();
+            Debug.Log("inSpace");
+        }
+	}
+
+    void launchBaseMusique()
+    {
+        source.clip = baseMusique;
+        source.Play();
+    }
+
+    void lauchSpaceMusique()
+    {
+        if (SpaceMusics.Count >= 1)
+        {
+            source.clip = SpaceMusics[Random.Range(0, SpaceMusics.Count)];
+        }
+        source.Play();
+    }
+
+    bool testInBase()
+    {
+        return (SceneManager.GetActiveScene().name == "Bridge" || SceneManager.GetActiveScene().name == "Factory" || SceneManager.GetActiveScene().name == "StorageBay");
+    }
+}
