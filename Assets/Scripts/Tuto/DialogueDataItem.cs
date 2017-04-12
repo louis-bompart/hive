320edb007bfa89f9a51987421143570782c42b94
Aymeric-Duplaquet <aymeric.duplaquet1@uqac.ca>
2017-04-11 22:38:07 -0400

    Tuto work (#68)
    * Dialogue fixes, dialogue database dialogue endline + State Machine and Tuto state
    
    * Dialogue wait list and StateManager prefab and tests
    
    * Tuto scripté

---
+++ Assets/Scripts/Tuto/DialogueDataItem.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueDataItem
+{
+    public string _ID;
+    public string _Text;
+    public float _displayTime;
+
+
+    public string ID
+    {
+        get
+        {
+            return _ID;
+        }
+        set
+        {
+            _ID = value;
+        }
+    }
+    public string Text
+    {
+        get
+        {
+            return _Text;
+        }
+        set
+        {
+            _Text = value;
+        }
+    }
+    public float displayTime
+    {
+        get
+        {
+            return _displayTime;
+        }
+        set
+        {
+            _displayTime = value;
+        }
+    }
+
+
+    public DialogueDataItem(string ID,string Text,float displayTime )
+    {
+        _ID = ID;
+        _Text = Text;
+        _displayTime = displayTime;
+    }
+
+
+    public DialogueDataItem()
+    {
+        this.ID = "null";
+    }
+
+    public override bool Equals(object obj)
+    {
+        return (obj as DialogueDataItem).ID == this.ID;
+    }
+    public override int GetHashCode()
+    {
+        return ID.GetHashCode();
+    }
+
+}
